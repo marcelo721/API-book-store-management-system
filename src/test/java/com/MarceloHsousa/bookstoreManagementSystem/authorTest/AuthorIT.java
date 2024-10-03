c667c5529a56670f0017accaefe54213fd351055
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-10-03 16:50:04 -0300

    Refactored AuthorIT test class

--- src/test/java/com/MarceloHsousa/bookstoreManagementSystem/authorTest/AuthorIT.java
+++ src/test/java/com/MarceloHsousa/bookstoreManagementSystem/authorTest/AuthorIT.java
@@ -1,13 +1,13 @@
 package com.MarceloHsousa.bookstoreManagementSystem.authorTest;
 
 
+import com.MarceloHsousa.bookstoreManagementSystem.userTest.JwtAuthentication;
 import com.MarceloHsousa.bookstoreManagementSystem.web.dto.authorDto.AuthorCreateDto;
 import com.MarceloHsousa.bookstoreManagementSystem.web.dto.authorDto.AuthorResponseDto;
 import com.MarceloHsousa.bookstoreManagementSystem.web.dto.authorDto.AuthorUpdateDto;
 import com.MarceloHsousa.bookstoreManagementSystem.web.exceptions.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -34,6 +34,7 @@ public class AuthorIT {
         AuthorResponseDto response = testClient
                 .post()
                 .uri("/api/v1/authors")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorCreateDto)
                 .exchange()
@@ -45,7 +46,7 @@ public class AuthorIT {
         org.assertj.core.api.Assertions.assertThat(response.getId()).isNotNull();
         org.assertj.core.api.Assertions.assertThat(response.getName()).isEqualTo("George miller");
         org.assertj.core.api.Assertions.assertThat(response.getNationality()).isEqualTo("Americano");
-    }
+    }//
 
     @Test
     public void createAuthor_withInvalidData_returnStatus422(){
@@ -57,6 +58,7 @@ public class AuthorIT {
         ErrorMessage response = testClient
                 .post()
                 .uri("/api/v1/authors")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorCreateDto)
                 .exchange()
@@ -75,7 +77,8 @@ public class AuthorIT {
          response = testClient
                 .post()
                 .uri("/api/v1/authors")
-                .contentType(MediaType.APPLICATION_JSON)
+                 .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
+                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorCreateDto)
                 .exchange()
                 .expectStatus().isEqualTo(422)
@@ -93,7 +96,8 @@ public class AuthorIT {
          response = testClient
                 .post()
                 .uri("/api/v1/authors")
-                .contentType(MediaType.APPLICATION_JSON)
+                 .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
+                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorCreateDto)
                 .exchange()
                 .expectStatus().isEqualTo(422)
@@ -111,7 +115,8 @@ public class AuthorIT {
          response = testClient
                 .post()
                 .uri("/api/v1/authors")
-                .contentType(MediaType.APPLICATION_JSON)
+                 .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
+                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorCreateDto)
                 .exchange()
                 .expectStatus().isEqualTo(422)
@@ -121,13 +126,57 @@ public class AuthorIT {
         org.assertj.core.api.Assertions.assertThat(response).isNotNull();
         org.assertj.core.api.Assertions.assertThat(response.getStatus()).isEqualTo(422);
 
-    }
+    }//
+
+    @Test
+    public void createAuthor_withoutPermission_returnStatus403() {
+        AuthorCreateDto authorCreateDto = AuthorCreateDto.builder()
+                .name("George miller")
+                .nationality("Americano")
+                .birthDate(LocalDate.of(1999, 12, 12)).build();
+
+        ErrorMessage response = testClient
+                .post()
+                .uri("/api/v1/authors")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "marceloHenrique@gmail.com", "12345678"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(authorCreateDto)
+                .exchange()
+                .expectStatus().isEqualTo(403)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(response).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(response.getStatus()).isEqualTo(403);
+    }//
+
+    @Test
+    public void createAuthor_notAuthenticated_returnStatus401() {
+        AuthorCreateDto authorCreateDto = AuthorCreateDto.builder()
+                .name("marcelo")
+                .nationality("Americano")
+                .birthDate(LocalDate.of(1999, 12, 12)).build();
+
+        ErrorMessage response = testClient
+                .post()
+                .uri("/api/v1/authors")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(authorCreateDto)
+                .exchange()
+                .expectStatus().isEqualTo(401)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(response).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(response.getStatus()).isEqualTo(401);
+    }//
 
     @Test
     public void findAuthor_withInvalidId_returnStatus404(){
         ErrorMessage responseBody = testClient
                 .get()
                 .uri("/api/v1/authors/0")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .exchange()
                 .expectStatus().isNotFound()
                 .expectBody(ErrorMessage.class)
@@ -135,13 +184,14 @@ public class AuthorIT {
 
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(404);
-    }
+    }//
 
     @Test
     public void findAuthor_withValidId_returnStatus200() {
         AuthorResponseDto responseBody = testClient
                 .get()
                 .uri("/api/v1/authors/1")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(AuthorResponseDto.class)
@@ -150,7 +200,36 @@ public class AuthorIT {
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.getName()).isEqualTo("George");
         org.assertj.core.api.Assertions.assertThat(responseBody.getNationality()).isEqualTo("alemão");
-    }
+    }//
+
+    @Test
+    public void findAuthor_withoutPermission_returnStatus403(){
+        ErrorMessage responseBody = testClient
+                .get()
+                .uri("/api/v1/authors/0")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "marceloHenrique@gmail.com", "12345678"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
+    }//
+
+    @Test
+    public void findAuthor_notAuthenticated_returnStatus401(){
+        ErrorMessage responseBody = testClient
+                .get()
+                .uri("/api/v1/authors/0")
+                .exchange()
+                .expectStatus().isEqualTo(401)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(401);
+    }//
 
     @Test
     public void findAllAuthors_withoutParameters_returnStatus200(){
@@ -158,21 +237,56 @@ public class AuthorIT {
         List<AuthorResponseDto> responseBody = testClient
                 .get()
                 .uri("/api/v1/authors")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .exchange()
                 .expectStatus().isOk()
                 .expectBodyList(AuthorResponseDto.class)
                 .returnResult().getResponseBody();
 
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
-        org.assertj.core.api.Assertions.assertThat(responseBody.size()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(responseBody.size()).isEqualTo(4);
 
-    }
+    }//
+
+    @Test
+    public void findAllAuthors_withoutPermission_returnStatus403(){
+
+        ErrorMessage responseBody = testClient
+                .get()
+                .uri("/api/v1/authors")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "marceloHenrique@gmail.com", "12345678"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
+
+    }//
+
+    @Test
+    public void findAllAuthors_notAuthenticated_returnStatus401(){
+
+        ErrorMessage responseBody = testClient
+                .get()
+                .uri("/api/v1/authors")
+                .exchange()
+                .expectStatus().isEqualTo(401)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(401);
+
+    }//
 
     @Test
     public void deleteAuthor_withValidId_returnStatus204(){
         testClient
                 .delete()
                 .uri("/api/v1/authors/2")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .exchange()
                 .expectStatus().isNoContent()
                 .expectBody().isEmpty();
@@ -180,9 +294,10 @@ public class AuthorIT {
         testClient
                 .delete()
                 .uri("/api/v1/authors/2")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .exchange()
                 .expectStatus().isNotFound();
-    }
+    }//
 
     @Test
     public void deleteAuthor_withInvalidId_returnStatus404(){
@@ -190,6 +305,7 @@ public class AuthorIT {
         ErrorMessage responseBody = testClient
                 .delete()
                 .uri("/api/v1/authors/0")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .exchange()
 
                 .expectStatus().isEqualTo(404)
@@ -198,13 +314,14 @@ public class AuthorIT {
 
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(404);
-    }
+    }//
 
     @Test
     public void deleteAuthor_associatedWithOtherEntity_returnStatus409(){
         ErrorMessage responseBody = testClient
                 .delete()
                 .uri("/api/v1/authors/1")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .exchange()
                 .expectStatus().isEqualTo(409)
                 .expectBody(ErrorMessage.class)
@@ -213,7 +330,40 @@ public class AuthorIT {
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
 
-    }
+    }//
+
+    @Test
+    public void deleteAuthor_withoutPermission_returnStatus403(){
+
+        ErrorMessage responseBody = testClient
+                .delete()
+                .uri("/api/v1/authors/0")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "marceloHenrique@gmail.com", "12345678"))
+                .exchange()
+
+                .expectStatus().isEqualTo(403)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
+    }//
+
+    @Test
+    public void deleteAuthor_notAuthenticated_returnStatus401(){
+
+        ErrorMessage responseBody = testClient
+                .delete()
+                .uri("/api/v1/authors/0")
+                .exchange()
+
+                .expectStatus().isEqualTo(401)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(401);
+    }//
 
     @Test
     public void updateAuthor_withValidIdAndValidData_returnStatus200(){
@@ -226,6 +376,7 @@ public class AuthorIT {
         AuthorResponseDto responseBody = testClient
                 .put()
                 .uri("/api/v1/authors/3")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorUpdateDto)
                 .exchange()
@@ -237,7 +388,7 @@ public class AuthorIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getNationality()).isEqualTo("ingles");
         org.assertj.core.api.Assertions.assertThat(responseBody.getName()).isEqualTo("joao carlos");
         org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isEqualTo(3);
-    }
+    }//
 
     @Test
     public void updateAuthor_withInvalidId_returnStatus404(){
@@ -250,6 +401,7 @@ public class AuthorIT {
         ErrorMessage response = testClient
                 .put()
                 .uri("/api/v1/authors/0")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorUpdateDto)
                 .exchange()
@@ -261,7 +413,7 @@ public class AuthorIT {
         org.assertj.core.api.Assertions.assertThat(response).isNotNull();
         org.assertj.core.api.Assertions.assertThat(response.getStatus()).isEqualTo(404);
 
-    }
+    }//
 
     @Test
     public void updateAuthor_withInvalidData_returnStatus422(){
@@ -274,6 +426,7 @@ public class AuthorIT {
         ErrorMessage responseBody = testClient
                 .put()
                 .uri("/api/v1/authors/3")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorUpdateDto)
                 .exchange()
@@ -292,7 +445,8 @@ public class AuthorIT {
          responseBody = testClient
                 .put()
                 .uri("/api/v1/authors/3")
-                .contentType(MediaType.APPLICATION_JSON)
+                 .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
+                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorUpdateDto)
                 .exchange()
                 .expectStatus().isEqualTo(422)
@@ -310,7 +464,8 @@ public class AuthorIT {
          responseBody = testClient
                 .put()
                 .uri("/api/v1/authors/3")
-                .contentType(MediaType.APPLICATION_JSON)
+                 .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
+                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorUpdateDto)
                 .exchange()
                 .expectStatus().isEqualTo(422)
@@ -328,7 +483,8 @@ public class AuthorIT {
          responseBody = testClient
                 .put()
                 .uri("/api/v1/authors/3")
-                .contentType(MediaType.APPLICATION_JSON)
+                 .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@gmail.com", "12345678"))
+                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(authorUpdateDto)
                 .exchange()
                 .expectStatus().isEqualTo(422)
@@ -337,5 +493,54 @@ public class AuthorIT {
 
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
-    }
+    }//
+
+    @Test
+    public void updateAuthor_withoutPermission_returnStatus403(){
+
+        AuthorUpdateDto authorUpdateDto = AuthorUpdateDto.builder()
+                .nationality("ingles")
+                .name("joao carlos")
+                .birthDate(LocalDate.of(1999, 12,11)).build();
+
+        ErrorMessage response = testClient
+                .put()
+                .uri("/api/v1/authors/0")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "marceloHenrique@gmail.com", "12345678"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(authorUpdateDto)
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody(ErrorMessage.class)
+                .returnResult()
+                .getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(response).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(response.getStatus()).isEqualTo(403);
+
+    }//
+
+    @Test
+    public void updateAuthor_notAuthenticated_returnStatus401(){
+
+        AuthorUpdateDto authorUpdateDto = AuthorUpdateDto.builder()
+                .nationality("ingles")
+                .name("joao carlos")
+                .birthDate(LocalDate.of(1999, 12,11)).build();
+
+        ErrorMessage response = testClient
+                .put()
+                .uri("/api/v1/authors/0")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(authorUpdateDto)
+                .exchange()
+                .expectStatus().isUnauthorized()
+                .expectBody(ErrorMessage.class)
+                .returnResult()
+                .getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(response).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(response.getStatus()).isEqualTo(401);
+
+    }//
 }
